d49ea329c4a5ebd15c55f411b84e45ac5264cd9e
Maciej Dziąg <maciejd0@op.pl>
2025-01-28 18:00:28 +0100

    s

--- src/main.js
+++ src/main.js
@@ -1,20 +1,19 @@
-import QrScanner from 'qr-scanner';
-/**
- * 
- * @param {string} decodedText 
- * @param {*} _decodedResult 
- */
-async function onScanSuccess(decodedText) {
-   const code = decodedText.split(' ')[0].trim()
-   console.log(code)
-   document.getElementById("qr-text").innerText = decodedText
-}
-const qrScanner = new QrScanner(
-    document.querySelector('video'),
-    onScanSuccess,
-    { },
-);
+import {Html5Qrcode} from 'html5-qrcode'
 
-document.getElementById('start').addEventListener('click', ()=>{
-    qrScanner.start()
-})
+const html5QrCode = new Html5Qrcode("reader");
+
+  function startScanner() {
+    const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+      document.getElementById("result").innerText = `Scanned: ${decodedText}`;
+    };
+    html5QrCode.start(
+      { facingMode: "environment" },
+      { fps: 10, qrbox: { width: 250, height: 250 } },
+      qrCodeSuccessCallback
+    ).catch((err) => {
+      console.error("Error starting the scanner:", err);
+    });
+  }
+
+  // Start the scanner when the page loads
+  startScanner();
