4b56537f2adabe3e31c35a4c0cd0098daf9904ae
Maciej Dziąg <maciejd0@op.pl>
2025-01-28 17:16:18 +0100

    f

--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
-import { Html5QrcodeScanner } from "html5-qrcode";
-
+import QrScanner from 'qr-scanner';
 /**
  * 
  * @param {string} decodedText 
  * @param {*} _decodedResult 
  */
-async function onScanSuccess(decodedText, _decodedResult) {
+async function onScanSuccess(decodedText) {
    const code = decodedText.split(' ')[0].trim()
    console.log(code)
    document.getElementById("qr-text").innerText = decodedText
 }
-
-function onError(errorMessage) {
-  //console.error(`Error scanning QR code: ${errorMessage}`);
-}
-
-var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 10, qrbox: 250, rememberLastUsedCamera: true, supportedScanTypes: [0] }, false);
-html5QrcodeScanner.render(onScanSuccess, onError);
+const qrScanner = new QrScanner(
+    document.querySelector('video'),
+    onScanSuccess,
+    { },
+);
